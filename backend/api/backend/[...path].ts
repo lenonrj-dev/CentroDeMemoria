ae05cfc2be1b436a02fb0ff2fffcd44acc40b2dd
Lenonrjdev <lenon.contato.dev.co@gmail.com>
2026-01-10 15:14:38 -0300

    Correção de CORS Origin do Admin

---
+++ backend/api/backend/[...path].ts
@@ -0,0 +1,6 @@
+import type { IncomingMessage, ServerResponse } from "node:http";
+import { handleRequest } from "../../src/serverless";
+
+export default async function handler(req: IncomingMessage, res: ServerResponse) {
+  return handleRequest(req, res);
+}
